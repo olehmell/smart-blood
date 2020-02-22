c47b9c33412906d9e22030709b2fae6899dae9b2
Mell <oleh.melnechyk@gmail.com>
2020-02-22 18:54:38 +0200

    Initial react-app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import './App.scss';
+import { Button } from 'antd';
+
+function App() {
+  return (
+  <>
+    <h1>Smart blood</h1>
+    <Button>Button</Button>
+    <input className='input'/>
+  </>
+  );
+}
+
+export default App;
