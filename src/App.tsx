c5098ad9c7d1296baabd702e269a59d5c47051ae
Mell <oleh.melnechyk@gmail.com>
2020-02-23 07:15:31 +0200

    Add centerStats and points

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
 import './App.scss';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Points from './pages/Points';
 import Donation from './pages/Donation';
+import RetipientsList from './pages/RetipientsList';
 
 import Header from './components/Header';
 
@@ -15,6 +17,8 @@ const App: React.FC = () => {
             <Route exact path='/' component={Home} />
             <Route path='/profile' component={Profile} />
             <Route path='/donation' component={Donation} />
+            <Route path='/retipientlist' component={RetipientsList} />
+            <Route path='/points' component={Points} />
             <Redirect to="/" />
           </Switch>
       </Header>
